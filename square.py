0f54d2a19fdf18acd93ae342de1dbdf9ab00cc58
sakilanasrinsetu <setusakilanasrin@gmail.com>
2022-10-05 22:48:48 +0600

    big O(n) notation comment modify

--- square.py
+++ square.py
@@ -6,7 +6,7 @@ def square_number_list(number_list):
     
     print("Original Value list = " ,number_list, "\nSquare Value list = " ,square_value_list)
 
-# This is a BigO or O(n) Notation
+# This is a Big O or O(n) Notation
 
 number_list = [2,5,6,10,61]
 square_number_list(number_list)
